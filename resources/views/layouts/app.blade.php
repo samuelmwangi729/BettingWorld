d961b3cfcca0c18bea044c77fa4d823f566e3595
samuel mwangi <samuelmwangi729@gmail.com>
2020-07-27 09:17:56 +0300

    final

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -164,7 +164,7 @@
 			</div>	
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-					<p class="h6">© All right Reversed.<a class="text-green ml-2" href="/" target="_blank">BettingWorld</a></p>
+                    <p>{{ config('app.name') }} © Copyrights  <?php echo date('Y');?> {{ config('app.name') }}| All Rights Reserved &nbsp; Design by <a href="mailto:samuelmwangi729@gmail.com" style="color:white;text-style:none !mportant">Samuel Mwangi</a></p>
 				</div>
 				<hr>
 			</div>	
